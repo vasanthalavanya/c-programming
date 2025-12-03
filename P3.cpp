ad13606c75a5476a0f4539020ca1f0efbbc5b61d
vasanthalavanya <sakevasanthalavanya230@gmail.com>
2025-12-03 21:03:03 +0530

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,13 @@
+//Even or Odd
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("natural number:");
+	scanf("%i",&n);
+	if(n%2==0)
+	printf("even number");
+	else
+	printf("odd number");
+	return 0;
+}
