ca3c9c36f548ddfdea02cc67a8ded872c0d40cad
vasanthalavanya <sakevasanthalavanya230@gmail.com>
2025-12-03 21:03:53 +0530

    Add files via upload

---
+++ P4.cpp
@@ -0,0 +1,20 @@
+//int,flaot,char,string and display
+#include<stdio.h>
+int main()
+{
+	int num;
+	float value;
+	char grade;
+	char name[50];
+	printf("enter a num");
+	scanf("%d",&num);
+	printf("enter n value");
+	scanf("%f",&num);
+	printf("enter grade");
+	scanf("%c",&grade);
+	printf("enter name");
+	scanf("%s",&name);
+	printf("%d\n%f\n%\n%s,num,n,grade,name");
+	return 0;
+
+}
