8acc48dc02d3db2d41347e57edb4e8fcdcc7076c
vasanthalavanya <sakevasanthalavanya230@gmail.com>
2025-12-04 14:14:27 +0530

    Add files via upload

---
+++ sum and difference of two numbers.cpp
@@ -0,0 +1,11 @@
+ //Write a C program to find the sum and difference of two numbers. 
+#include <stdio.h> 
+int main()
+{ 
+int a, b; 
+printf("Enter two numbers: "); 
+scanf("%d %d", &a, &b); 
+printf("Sum = %d\n", a + b); 
+printf("Difference = %d\n", a - b); 
+return 0; 
+}
